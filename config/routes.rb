e8f724dfb847537c5ebd614d676d7a9038e9b53f
AlbertoJTD <albertojtd@outlook.com>
2023-02-08 16:03:44 -0600

    Namespace added for API version

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
-  root "users#index"
+  root "api/v1/users#index"
+  
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
 end
